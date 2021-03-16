01430618f15b2b49fa745d921d5ce2f49d3fd714
Bruno Santos <brunosansp@outlook.com>
2021-03-16 07:33:40 -0300

    aulas 01_10, jsx e css

---
+++ curso-react/fundamentos-react/src/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+// const el = document.getElementById('root')
+// ReactDOM.render('Olá React!', el)
+
+const tag = <strong>Olá React!</strong>
+ReactDOM.render(
+    <div>
+        { tag }
+    </div>,
+    document.getElementById('root')
+)
