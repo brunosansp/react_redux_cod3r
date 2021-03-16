2c2c5ebfc219605e9c475c157e0f6c7141ab4ad3
Bruno Santos <brunosansp@outlook.com>
2021-03-16 07:51:18 -0300

    add jsx in Primeiro component

--- curso-react/fundamentos-react/src/index.js
+++ curso-react/fundamentos-react/src/index.js
@@ -2,13 +2,16 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import Primeiro from './components/basicos/Primeiro'
+
 // const el = document.getElementById('root')
 // ReactDOM.render('Olá React!', el)
 
 const tag = <strong>Olá React!</strong>
 ReactDOM.render(
     <div>
-        { tag }
+        { tag }, 
+        <p><Primeiro></Primeiro></p>
     </div>,
     document.getElementById('root')
 )
